845fe8bd8573489e62f693039e446ba9adb655ac
zelima <ir.mchedlishvili@gmail.com>
2021-01-13 17:49:19 +0400

    Examples

--- search.py
+++ search.py
@@ -6,3 +6,10 @@
 # Create a function  that takes list and number as an argument and return appropriate boolian and print
 
 # Once done with the task, create a Pull Request on https://github.com/zelima/element-search
+
+# Useful Example
+# for something in [1,2,3,4,5,6,7,8,98,9, 'hello']:
+#     print(something)
+
+# for num in range(25):
+#     print(num)
