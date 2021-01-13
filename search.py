c632790436e9fc3f4da196f539f8ea7c10221d03
zelima <ir.mchedlishvili@gmail.com>
2021-01-13 17:36:42 +0400

    Initial Commit

---
+++ search.py
@@ -0,0 +1,6 @@
+# https://www.practicepython.org/exercise/2014/11/11/20-element-search.html
+
+# User gives list of [1,4,5,12,16,45] and Number 120 as an input.
+# User gives list of [1,4,5,12,16,45] and Number 16 as an input.
+
+# Create a function  that takes list and number as an argument and return appropriate boolian and print
